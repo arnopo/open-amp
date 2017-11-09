c38528c496ede4111aed41056babe8e810173da3
Arnaud Pouliquen <arnaud.pouliquen@st.com>
2017-11-09 11:38:59 +0100

    cmake options: change M4 option
    change option for mfloat-abi to Hard method
    
    Change-Id: I9a18fa034d10511dbf78445f6a70ad2f44771c9c

--- cmake/platforms/stm32mp1-m4-generic.cmake
+++ cmake/platforms/stm32mp1-m4-generic.cmake
@@ -1,6 +1,6 @@
 set (CMAKE_SYSTEM_PROCESSOR "arm" CACHE STRING "")
 set (MACHINE "stm32mp1_m4" CACHE STRING "")
 
-set (CMAKE_C_FLAGS          "-mcpu=cortex-m4 -mthumb -mfloat-abi=softfp -mfpu=fpv4-sp-d16 " CACHE STRING "")
+set (CMAKE_C_FLAGS          "-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 " CACHE STRING "")
 
 include (cross_generic_gcc)
