8fa333815f3446347f5b399574ef862b89de8310
Arnaud Pouliquen <arnaud.pouliquen@st.com>
2018-01-10 16:12:06 +0100

    create cmake file for cortex M4
    Change-Id: I89dc3c1139d2b1cfa714ff8cb0b6727839eda720

---
+++ cmake/platforms/cortex_m4_generic.cmake
@@ -0,0 +1,6 @@
+set (CMAKE_SYSTEM_PROCESSOR "arm" CACHE STRING "")
+set (MACHINE "cortexm" CACHE STRING "")
+
+set (CMAKE_C_FLAGS "-mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -Og -fmessage-length=0 -ffunction-sections -c" CACHE STRING "")
+
+include (cross_generic_gcc)
